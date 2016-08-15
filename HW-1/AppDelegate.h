941fc4f5345f002f88c9d8996cbd4511bd17406f
Мария Мирошниченко <Maria@MacBook-Air-Maria.local>
2016-08-15 11:41:59 +0300

    first commit

---
+++ HW-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW-1
+//
+//  Created by Мария Мирошниченко on 15.08.16.
+//  Copyright © 2016 GB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW-1
+//
+//  Created by Мария Мирошниченко on 15.08.16.
+//  Copyright © 2016 GB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
